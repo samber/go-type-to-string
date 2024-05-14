87dd1da426ebae3ed4f7f846c6e108b2cc1d22f9
d-enk <43112758+d-enk@users.noreply.github.com>
2024-05-14 22:16:48 +0300

    fix: named type more priority (#6)

--- converter.go
+++ converter.go
@@ -25,6 +25,17 @@ func GetReflectValueType(v reflect.Value) string {
 // getType generates a service name from a type.
 // It returns a string like "*[]*github.com/samber/example.test".
 func getType(typeOfT reflect.Type) string {
+	if name := typeOfT.Name(); name != "" {
+
+		pkgPath := typeOfT.PkgPath()
+		if pkgPath == "" {
+			// builtin type
+			return name
+		}
+
+		return pkgPath + "." + name
+	}
+
 	switch typeOfT.Kind() {
 	case reflect.Pointer:
 		return "*" + getType(typeOfT.Elem())
@@ -51,19 +62,11 @@ func getType(typeOfT reflect.Type) string {
 		return fmt.Sprintf("%s %s", prefix, getType(typeOfT.Elem()))
 	case reflect.Func:
 		// @TODO: handle arguments and returned types recursively
-		return typeOfT.String()
-	}
 
-	pkgPath := typeOfT.PkgPath()
-	if pkgPath == "" {
-		// anonymous type
 		return typeOfT.String()
-	}
 
-	if typeOfT.Name() == "" {
+	default:
 		// any + interface{} + anonymous type
 		return typeOfT.String()
 	}
-
-	return pkgPath + "." + typeOfT.Name()
 }
